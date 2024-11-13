69541170e966dcfb3b28bdde5fade984f8c616ec
Macbook <akindele@omnnixbms.com>
2024-11-13 11:26:35 +0100

    build: add compiled TypeScript files to build folder for Heroku deployment

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import app from './app';
 
+// Root route to respond to GET requests at the root URL
+app.get('/', (req, res) => {
+  res.send('Welcome to the MVP wallet service');
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
