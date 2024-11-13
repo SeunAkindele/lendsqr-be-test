b8cf4f122f8da0fc5265e1e3e9231850610a2752
Macbook <akindele@omnnixbms.com>
2024-11-13 11:09:41 +0100

    build: add compiled TypeScript files to build folder for Heroku deployment

--- src/faux-auth-middleware.js
+++
@@ -1,9 +0,0 @@
-export function fauxAuthMiddleware(req, res, next) {
-    const token = req.headers['authorization'];
-    if (token && token === `Bearer ${process.env.FAUX_TOKEN}`) {
-        next(); // Proceed to the next middleware or route handler
-    }
-    else {
-        res.status(401).json({ message: 'Unauthorized: Invalid token' });
-    }
-}
