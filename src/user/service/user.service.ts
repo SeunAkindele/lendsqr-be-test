ea6b7607b573c28d3e7e0f8811a457ac43ac8f3d
Macbook <akindele@omnnixbms.com>
2024-12-05 14:55:26 +0100

    changed token to login

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -40,13 +40,13 @@ export default class UserService {
             if (!userData.name || !userData.email || !userData.password) {
                 throw new BadRequestError('All fields (name, email, password) must be filled');
             }
+            
             // checking if user is blacklisted
             // const isUserBlacklisted = await this.checkBlacklist(userData.email);
             // if(isUserBlacklisted) {
             //     return `This user is in the Lendsqr Adjutor Karma blacklist and cannot onboard`;
             // }
-            
-            
+
             const user = await this.findByEmail(userData.email);
             if(user) {
                 return 'This email already exist';
